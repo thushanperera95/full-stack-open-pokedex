72fd6d5e3921aa335c54e56dd22be1574ba1dc24
Thushan <thushan.perera95@gmail.com>
2022-06-25 16:58:13 +0800

    Reverted changes to cypress test file

--- cypress/integration/pokedex.cy.js
+++ cypress/integration/pokedex.cy.js
@@ -5,9 +5,9 @@ describe('Pokedex', function() {
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
 
-  it('clicking on pokemon in front page will redirect to pokemon details page', function() {
-    cy.visit('http://localhost:5000')
-    cy.contains('ivysaur').click()
-    cy.contains('chlorophyll')
-  })
+  // it('clicking on pokemon in front page will redirect to pokemon details page', function() {
+  //   cy.visit('http://localhost:5000')
+  //   cy.contains('ivysaur').click()
+  //   cy.contains('chlorophyll')
+  // })
 })
